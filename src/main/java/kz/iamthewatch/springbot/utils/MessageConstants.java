194e79d7d0ce1d3e37c2d8d3b0bab15973b2d2f6
iamthewatch <sagatfarhat@gmail.com>
2026-01-06 17:39:06 +0500

    added message constant

---
+++ src/main/java/kz/iamthewatch/springbot/utils/MessageConstants.java
@@ -0,0 +1,14 @@
+package kz.iamthewatch.springbot.utils;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public final class MessageConstants {
+
+    public static final String ABOUT_REQUEST = "menu.about";
+    public static final String BOUT_RESPONSE = "system.about";
+
+    public static final String CONSULTATION_REQUEST_ACCEPTED_RESPONSE = "consultation.request.accepted";
+    public static final String CONSULTATION_RESTART_RESPONSE = "consultation.restart";
+
+}
